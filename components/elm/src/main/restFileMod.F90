e6420d1eece1d2e09389f9afd208bd3e2833bf54
Benjamin Sulman <sulmanbn@ornl.gov>
2022-03-11 16:33:27 -0500

    Move chem state to ColumnDataType

--- components/elm/src/main/restFileMod.F90
+++ components/elm/src/main/restFileMod.F90
@@ -65,6 +65,7 @@ module restFileMod
   use VegetationDataType   , only : veg_ps, veg_pf
   use GridcellDataType     , only : grc_cs, grc_ws 
   use elm_instMod          , only : chemstate_vars
+  use ColumnDataType       , only : col_chem
   
   !
   ! !PUBLIC TYPES:
@@ -288,7 +289,7 @@ contains
     endif
 
     if (use_alquimia) then
-      call chemstate_vars%Restart(bounds, ncid, flag='define')
+      call col_chem%Restart(bounds, ncid, flag='define')
     endif
 
     if (present(rdate)) then 
@@ -432,7 +433,7 @@ contains
     endif
 
     if (use_alquimia) then
-      call chemstate_vars%Restart(bounds, ncid, flag='write')
+      call col_chem%Restart(bounds, ncid, flag='write')
     endif
 
     call hist_restart_ncd (bounds, ncid, flag='write' )
@@ -661,7 +662,7 @@ contains
     endif
 
     if (use_alquimia) then
-      call chemstate_vars%Restart(bounds, ncid, flag='read')
+      call col_chem%Restart(bounds, ncid, flag='read')
     endif
         
     call hist_restart_ncd (bounds, ncid, flag='read')
